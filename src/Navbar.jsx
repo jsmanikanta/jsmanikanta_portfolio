fa14a1eff5cfab8631468dd0b29f03e7fea95336
J S Manikanta <jsmanikanta13@gmail.com>
2026-02-10 10:01:20 +0530

    done

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,22 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./styles/navbar.css";
 
 export default function Navbar() {
+  const [open, setOpen] = useState(false);
+
   return (
     <header>
       <nav>
         <div className="left">J S Manikanta&apos;s Portfolio</div>
 
         <div className="right">
+          {/* Desktop links */}
           <ul>
-            <li><NavLink to="/" end>Home</NavLink></li>
-            <li><NavLink to="/about">About</NavLink></li>
-            <li><NavLink to="/experience">Experience</NavLink></li>
-            <li><NavLink to="/projects">Projects</NavLink></li>
+            <li>
+              <NavLink to="/" end>
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">About</NavLink>
+            </li>
+            <li>
+              <NavLink to="/experience">Experience</NavLink>
+            </li>
+            <li>
+              <NavLink to="/projects">Projects</NavLink>
+            </li>
           </ul>
+
+          {/* Mobile button */}
+          <button className="menuBtn" onClick={() => setOpen((v) => !v)}>
+            {open ? "Close" : "Menu"}
+          </button>
         </div>
       </nav>
+
+      {/* Mobile dropdown */}
+      {open && (
+        <div className="mobilePanel">
+          <ul>
+            <li>
+              <NavLink to="/" end onClick={() => setOpen(false)}>
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/about" onClick={() => setOpen(false)}>
+                About
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/experience" onClick={() => setOpen(false)}>
+                Experience
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/projects" onClick={() => setOpen(false)}>
+                Projects
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      )}
     </header>
   );
 }
